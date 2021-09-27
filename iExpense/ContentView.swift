5c52791ae33dc2f01539f79bdbf6e17186c06390
1Soyebo <soyeboibukun@gmail.com>
2021-09-27 16:31:12 +0100

    identifiable protocol

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -12,20 +12,46 @@ struct User: Codable {
     var lastName:String
 }
 
+struct ExpenseItem:Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-
-
-    var body: some View {
-        Button("hq"){
-            let encoder = JSONEncoder()
-
-            if let data = try? encoder.encode(self.user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+    @ObservedObject var expenses = Expenses()
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+    var body: some View{
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
             }
+            
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
         }
     }
+
+    
 }
 
 
